d5609cec39b7831078bf769d9d51a1d868f64596
EC2 Default User <ec2-user@ip-172-31-26-44.eu-west-1.compute.internal>
2021-11-18 01:29:31 +0000

    ec2

--- ec2.tf
+++ ec2.tf
@@ -23,11 +23,11 @@ resource "aws_instance" "web" {
 }
 
 
-resource "aws_instance" "web" {
+resource "aws_instance" "web1" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "HelloWorld1"
   }
 }
